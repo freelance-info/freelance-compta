6e3d91ba47a602867dfb278e49fe4a7b5d71f2e6
ben <bcaure@gmail.com>
2020-12-10 23:20:36 +0100

    :bug: local dev: node.env undefined

--- src/reducers/csv.js
+++ src/reducers/csv.js
@@ -1,10 +1,5 @@
 import { sortByCol } from './sort';
 
-let remote;
-if (process.env.REACT_APP_MODE === 'electron') {
-  remote = window.require('electron').remote;
-}
-
 export function writeData(path, header, data) {
   const createCsvWriter = window.require('csv-writer').createObjectCsvWriter;
   const csvWriter = createCsvWriter({ path, header: header.map(col => ({ ...col, title: col.id })) });
@@ -54,6 +49,9 @@ export function save(filePath, lines, cols, setActionMessage) {
 
 // Write values to new file
 export function saveAs(currentFile, setCurrentFile, fileChange, lines, cols, setActionMessage) {
+
+  const remote = window.require('electron').remote;  
+
   return new Promise((resolve, reject) => {
     const filePath = remote.dialog.showSaveDialogSync(remote.getCurrentWindow(), {
       title: 'Sauvegarde du livre de recette',
@@ -81,6 +79,9 @@ export function saveAs(currentFile, setCurrentFile, fileChange, lines, cols, set
 export function open(
   currentFile, setCurrentFile, fileChange, setActionMessage, lines, cols,
 ) {
+
+  const remote = window.require('electron').remote;  
+
   return new Promise((resolve, reject) => {
     const defaultPath = currentFile;
     const filePath = remote.dialog.showOpenDialogSync(remote.getCurrentWindow(), {
