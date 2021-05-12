fe785cfd6025cf91510d2041986f0a053f47ba86
Benjamin Caure <bcaure@gmail.com>
2021-05-12 18:10:34 +0200

    :sparkles: compute quarters ✅ fix date tests

--- src/utils/date.js
+++ src/utils/date.js
@@ -27,17 +27,35 @@ export const findMaxDate = (dates, maxDefault) => dates.reduce((max, cur) => {
 export const getQuarter = date => Math.floor(new Date(date).getMonth() / 3) + 1;
 
 export const getQuarters = (minDate, maxDate) => {
-  const minYear = new Date(minDate).getYear();
-  const maxYear = new Date(maxDate).getYear();
+  const minYear = new Date(minDate).getFullYear();
+  const maxYear = new Date(maxDate).getFullYear();
 
-  const years = {};
+  const quarters = {};
   for (let currYear = minYear; currYear <= maxYear; currYear += 1) {
-    years[currYear] = [1, 2, 3, 4];
+    quarters[currYear] = [1, 2, 3, 4];
   }
 
   const minQuarter = getQuarter(minDate);
-  years[minYear] = years[minYear].filter(quarter => quarter >= minQuarter);
+  quarters[minYear] = quarters[minYear].filter(quarter => quarter >= minQuarter);
 
   const maxQuarter = getQuarter(maxDate);
-  years[maxYear] = years[maxYear].filter(quarter => quarter <= maxQuarter);
+  quarters[maxYear] = quarters[maxYear].filter(quarter => quarter <= maxQuarter);
+
+  return quarters;
+};
+
+// quarter format: 'yyyy-q', e.g. '2021-1'
+export const getStartDateOfQuarter = quarter => {
+  const year = quarter.substring(0, 4);
+  const quarterNumber = quarter.substr(5, 1);
+  const month = ((quarterNumber - 1) * 3);
+  return new Date(year, month, 2);
+};
+
+// quarter format: 'yyyy-q', e.g. '2021-1'
+export const getEndDateOfQuarter = quarter => {
+  const year = quarter.substring(0, 4);
+  const quarterNumber = quarter.substr(5, 1);
+  const month = ((quarterNumber - 1) * 3) + 3;
+  return new Date(year, month, 1);
 };
--- src/utils/date.test.js
+++ src/utils/date.test.js
@@ -1,12 +1,42 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { getQuarters } from './date';
+import { getQuarters, getStartDateOfQuarter, getEndDateOfQuarter } from './date';
 
 test('Get quarters different years', () => {
-  const quarters = getQuarters('2000-01-04', '2002-02-01');
-  expect(quarters).toBe({
+  const quarters = getQuarters('2000-04-04', '2002-02-01');
+  expect(quarters).toStrictEqual({
     2000: [2, 3, 4],
     2001: [1, 2, 3, 4],
     2002: [1],
   });
+});
+
+test('Get several quarters same year', () => {
+  const quarters = getQuarters('2000-01-04', '2000-06-01');
+  expect(quarters).toStrictEqual({ 2000: [1, 2] });
+});
+
+test('Get 1 single quarter', () => {
+  const quarters = getQuarters('2000-01-04', '2000-06-01');
+  expect(quarters).toStrictEqual({ 2000: [1, 2] });
+});
+
+test('Get start date of first quarter', () => {
+  const date = getStartDateOfQuarter('2000-1');
+  expect(date.toISOString().substring(0, 10)).toBe('2000-01-01');
+});
+
+test('Get start date of last quarter', () => {
+  const date = getStartDateOfQuarter('2000-4');
+  expect(date.toISOString().substring(0, 10)).toBe('2000-10-01');
+});
+
+test('Get end date of first quarter', () => {
+  const date = getEndDateOfQuarter('2000-1');
+  expect(date.toISOString().substring(0, 10)).toBe('2000-03-31');
+});
+
+test('Get end date of last quarter', () => {
+  const date = getEndDateOfQuarter('2000-4');
+  expect(date.toISOString().substring(0, 10)).toBe('2000-12-31');
 });
